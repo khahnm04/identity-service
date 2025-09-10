1c161ba267a0e21020b80769b3a2d70b802f55c2
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-09-11 00:32:39 +0700

    connect to database with Spring JPA and create CRUD API

---
+++ src/main/java/com/khahnm04/identityservice/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.khahnm04.identityservice.controller;
+
+import com.khahnm04.identityservice.dto.request.UserCreationRequest;
+import com.khahnm04.identityservice.dto.request.UserUpdateRequest;
+import com.khahnm04.identityservice.entity.User;
+import com.khahnm04.identityservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "user has been deleted";
+    }
+
+}
---
+++ src/main/java/com/khahnm04/identityservice/service/UserService.java
@@ -0,0 +1,50 @@
+package com.khahnm04.identityservice.service;
+
+import com.khahnm04.identityservice.dto.request.UserCreationRequest;
+import com.khahnm04.identityservice.dto.request.UserUpdateRequest;
+import com.khahnm04.identityservice.entity.User;
+import com.khahnm04.identityservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("user not found"));
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("user not found"));
+    }
+
+}
