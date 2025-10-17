34319ed8a1c0b935ceb5976b80b21cd7d1f1cf9c
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-10-17 10:39:51 +0700

    Isolation in Unit Test

--- src/test/java/com/khahnm04/identityservice/controller/UserControllerTest.java
+++ src/test/java/com/khahnm04/identityservice/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.khahnm04.identityservice.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.khahnm04.identityservice.dto.request.UserCreationRequest;
@@ -9,13 +8,13 @@ import com.khahnm04.identityservice.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,6 +25,7 @@ import java.time.LocalDate;
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
 
     @Autowired
