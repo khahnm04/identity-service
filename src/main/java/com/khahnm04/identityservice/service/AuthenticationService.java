cd2e6a95981e56b2171a39e7b7943c8a057389b9
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-10-22 08:50:57 +0700

    Tìm hiểu về Profile và biến môi trường

--- src/main/java/com/khahnm04/identityservice/service/AuthenticationService.java
+++ src/main/java/com/khahnm04/identityservice/service/AuthenticationService.java
@@ -74,6 +74,8 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        log.info("Sign key: {}", SIGNER_KEY);
+
         var user = userRepository
                 .findByUsername(request.getUsername())
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
