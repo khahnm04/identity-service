eca14d2213806cc2ba42d191e660ff1a3dd91d8d
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-11-26 16:36:33 +0700

    Sign in with Google

---
+++ identity-service-frontend/src/configurations/configuration.js
@@ -0,0 +1,5 @@
+export const OAuthConfig = {
+  clientId: "743399374222-d2lalq4m7v39ah6kmlmpp7hvosbdfe2f.apps.googleusercontent.com",
+  redirectUri: "http://localhost:3000/authenticate",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+};
